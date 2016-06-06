10b2aa90216622794d53205fd01012a750b100b0
Collin Mutembei <mutembei.collin@gmail.com>
2016-06-06 19:42:07 +0300

    [#120890657] create QR codes for commodities
    - user gets a QR code generated when adding a
      commodity
    - update settings anf urls to cater for the QR
      images
    - update requirements.txt

--- api/serializers.py
+++ api/serializers.py
@@ -45,11 +45,19 @@ class CommoditySerializer(serializers.ModelSerializer):
     uuid = serializers.UUIDField(read_only=True, format='hex')
     requestor = serializers.ReadOnlyField(source='requestor.username')
     price = serializers.DecimalField(max_digits=8, decimal_places=2)
-    description = serializers.CharField(allow_blank=True, required=False)
+    commodity_qr = serializers.ImageField(use_url=True, read_only=True)
 
     class Meta:
         model = Commodity
-        fields = ('url', 'uuid', 'name', 'description', 'requestor', 'price')
+        fields = (
+            'url',
+            'uuid',
+            'name',
+            'description',
+            'requestor',
+            'price',
+            'commodity_qr'
+        )
         extra_kwargs = {
             'url': {'lookup_field': 'uuid'}
         }
