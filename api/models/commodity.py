8e8b4be38d3854d33857b2230245ee78140b9ac6
Collin Mutembei <mutembei.collin@gmail.com>
2016-06-08 10:32:04 +0300

    Revert "QR #120890657"

--- api/models/commodity.py
+++ api/models/commodity.py
@@ -1,34 +1,12 @@
 from django.db import models
-from django.core.files.uploadedfile import InMemoryUploadedFile
-import uuid
-import base64
-import time
-from io import BytesIO
-import pyqrcode
-from PIL import Image
-from user import User
-
-
-def upload_path(instance, filename):
-    """sets the directory for storing QR codes
-    """
-
-    requestor = User.objects.get(uuid=instance.requestor.uuid)
-    requestor_hex = requestor.uuid.hex
-    file_path = "commodities/{0}/{1}".format(requestor_hex[-20:], filename)
-    return file_path
+from api.models.user import User
 
 
 class Commodity(models.Model):
     """Commodity model defining item to be co-buyied
     """
 
-    uuid = models.UUIDField(
-        unique=True,
-        default=uuid.uuid4,
-        editable=False
-    )
-    requestor = models.ForeignKey(User, to_field='uuid')
+    requestor = models.ForeignKey(User)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     name = models.CharField(max_length=70)
     description = models.TextField()
@@ -36,39 +14,6 @@ class Commodity(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
     funded = models.BooleanField(default=False)
     date_funded = models.DateField(blank=True, null=True)
-    commodity_qr = models.ImageField(upload_to=upload_path, blank=True)
-
-    def save(self, **kwargs):
-        """overrides the save method for the model
-        """
-        request = pyqrcode.create(
-            '{0}.{1}'.format(str(self.uuid), self.price),
-            version=10
-            )
-        encoded_request = request.png_as_base64_str()
-        image = Image.open(
-            BytesIO(base64.b64decode(encoded_request.encode()))
-            )
-        filename = '{0}.{1}'.format(
-            str(time.time())[:10],
-            (image.format).lower()
-        )
-        memory_image = BytesIO()
-        image.save(memory_image, format=image.format)
-        qr_file = InMemoryUploadedFile(
-            memory_image,
-            None,
-            filename,
-            'image/png',
-            memory_image.tell,
-            None
-        )
-        self.commodity_qr.save(
-            filename,
-            qr_file,
-            save=False,
-        )
-        super(Commodity, self).save(**kwargs)
 
     def __unicode__(self):
         return '{0} {1} {2}'.format(self.requestor, self.name, self.price)
