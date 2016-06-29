af79b8fc0c708f5d2028393f69fb8cadc334c3de
Collin Mutembei <mutembei.collin@gmail.com>
2016-06-29 13:10:24 +0300

    [#119641193] support setting environment variables
    - add .env-example to indicate the required variables
    - refactor settings.py to pick variables from .env
    - update requirements.txt

--- tubuy/settings.py
+++ tubuy/settings.py
@@ -13,10 +13,13 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 import os
 import datetime
 import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Read env variables from a .env file in the root directory
+dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
