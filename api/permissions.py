c99558df28572bd801bbbb0752aedd99306acd23
Collin Mutembei <mutembei.collin@gmail.com>
2016-09-14 23:35:32 +0300

    [#130394679] Allow user to update their profile
    - Let /api/users/ list all users even when not admin user
    - Allow user to update their own profiles only

--- api/permissions.py
+++ api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsStaffOrTargetUser(permissions.BasePermission):
     def has_permission(self, request, view):
-        return view.action == 'retrieve' or request.user.is_superuser
+        return view.action == 'retrieve' or request.user
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_superuser or obj == request.user
