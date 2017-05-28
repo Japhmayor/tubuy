929ece1cb567a701174913633818fc27b9b93b8f
Collin Mutembei <mutembei.collin@gmail.com>
2017-05-29 00:46:18 +0300

    :checkered_flag: Add test to confirm commodity is funded
    - If a commodity's remaining amount is <= 0, then it's funded
    - #23

--- tests/test_commodity.py
+++ tests/test_commodity.py
@@ -44,3 +44,53 @@ class APIUsersTestCase(APITestCase):
             response.data.get('price'),
             ["Ensure this value is greater than or equal to 0.01."]
         )
+
+    def test_commodity_is_funded_if_remaining_amount_is_no_more_that_zero(self):
+        """ a commodity can only be funded if the remaining_amount is <= 0
+        """
+        commodity =  {
+            "name": "test commodity",
+            "description": "test commodity description",
+            "price": 100
+        }
+
+        commodity_response = self.client.post(
+            reverse('api:commodity-list'),
+            commodity
+        )
+
+        # make initial contribution
+
+        contribution = {
+            'contributing_to': commodity['name'],
+            'amount': 50
+        }
+        initial_contrib_response = self.client.post(
+            reverse('api:contribution-list'),
+            contribution,
+        )
+
+        self.assertEqual(initial_contrib_response.status_code, 201)
+
+        # check status of funding
+        funded_commodity_response = self.client.get(
+            reverse('api:commodity-detail', args=[commodity_response.data.get('uuid')])
+        )
+
+        self.assertEqual(funded_commodity_response.status_code, 200)
+        self.assertEqual(funded_commodity_response.data.get('funded'), False)
+        self.assertEqual(
+            funded_commodity_response.data.get('remaining_amount'),
+            "{:.2f}".format(commodity['price'] - contribution['amount'])
+        )
+
+        final_contrib_response = self.client.post(
+            reverse('api:contribution-list'),
+            contribution,
+        )
+
+        funded_commodity_response = self.client.get(
+            reverse('api:commodity-detail', args=[commodity_response.data.get('uuid')])
+        )
+
+        self.assertEqual(funded_commodity_response.data.get('funded'), True)
